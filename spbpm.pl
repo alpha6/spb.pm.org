94a51e305ba6d3560cfd942cbf7f070d64149ba5
Sergey Romanov <complefor@rambler.ru>
2011-11-22 18:34:06 +0400

    Minor fixes

--- spbpm.pl
+++ spbpm.pl
@@ -25,46 +25,44 @@ under sub {
         my $agent = $self->req->headers->user_agent || 'Empty';
         my $user = $session->find_one({ sid => $sid, sign => $agent });
         if ($user->{'last_act'} < (time - $session_life)) {
-            $self->stash( user_data => { is_auth => 0 });
+            $self->stash(user_data => { is_auth => 0 });
         }
         else {
             $user->{'is_auth'} = 1;
             delete($user->{'passwd'});
-            $self->stash( user_data => $user );
+            $self->stash(user_data => $user);
         }
     }
     else {
-        $self->stash( user_data => { is_auth => 0 } );
+        $self->stash(user_data => { is_auth => 0 });
     }
 
     1;
-
 };
 
 get '/' => sub {
 	my $self = shift;
-	$self->render( template => 'index');
+
+	$self->render(template => 'index');
 };
 
 get '/login' => sub {
     my $self = shift;
     
-    $self->render( template => 'login');
+    $self->render(template => 'login');
 };
 
 post '/login' => sub {
     my $self = shift;
-    my $login = $self->req->param('l');
-    my $passwd = $self->req->param('p');
-    
+
+    my $login  = $self->req->param('login');
+    my $passwd = $self->req->param('passwd');
     my $user = $users->find_one({ login => $login, passwd => sha1_hex($passwd) });
     if ($user->{'login'}) {
         my $sid = create_UUID_as_string(UUID_V4);
         my $agent = $self->req->headers->user_agent || 'Empty';
-
         $session->insert({ sid => $sid, sign => $agent, user => $user->{'login'}, last_act => time});
-        $self->session( sid => $sid );
-
+        $self->session(sid => $sid);
         $self->redirect_to('/');
     } else {
         $self->render(login_error => '<h4 class=error>Не правильный пароль</h4>', template => 'login');
@@ -73,13 +71,14 @@ post '/login' => sub {
 
 get '/register' => sub {
     my $self = shift;
-    $self->render( text => "<h3>А не работает еще :(</h3>", layout => 'index');
+
+    $self->render(text => "<h3>А не работает еще :(</h3>", layout => 'index');
 };
 
 get '/add/news' => sub {
     my $self = shift;
 
-    $self->render( template => 'add_news' );
+    $self->render(template => 'add_news');
 };
 
 post '/add/news' => sub {
@@ -88,9 +87,11 @@ post '/add/news' => sub {
 
 get '/users' => sub {
     my $self = shift;
+
     my @all = $users->find->sort({reg_date => 1})->all;
-    $self->render( text => "<pre>".Dumper(@all)."</pre>");
+    $self->render(text => "<pre>".Dumper(@all)."</pre>");
 };
 
 app->secret('Perl mongers of Saint-Petersburg');
 app->start;
+
